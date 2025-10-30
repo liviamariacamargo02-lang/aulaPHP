a9acc4a1839123f5c59b2db2a92c8e991534bc52
Etec <usuario@etec.com>
2025-10-30 12:05:33 -0300

    30/10

--- AulaBD/professor_cadastro.php
+++ AulaBD/professor_cadastro.php
@@ -3,16 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Professor Cadastro</title>
+    <title>Aluno Cadastro</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <h1> Cadastro de Professores </h1>
-    <form name="Enviar" method="POST" action="professor_cadastro.php">
-        <fieldset>
-            <p>Informe o nome do professor:</p>
-            <input type="text" name="professor">
+    <?php include "menu.php" ?>
 
-            <br>
-            <br>
-            <input type="submit" name="Enviar" value="Cadastrar Professor"> 
-        </fieldset>
+    <div class="conteudo">
+
+    <h3> Cadastro de Professores </h3>
+    <form action="#" method="post">
+
+    <p>
+        Digite o nome <br>
+        <input type="text" name="nome">
+</p>
+
+<p>
+        Digite a area de formação <br>
+        <input type="text" name="cidade">
+</p>
+
+<p>
+        Digite a graduação <br>
+        <input type="text" name="estado">
+</p>
+
+<p> 
+
+<input type="submit" name="Enviar" value="Cadastrar"> 
+
+</p>
+
+
+
+
+
+
+</form>
+
+</div>
+
+</body>
+</html>
+
---
+++ AulaBD/professor_consulta.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    
+<?php include "menu.php" ?>
+
+    <div class="conteudo">
+
+    <h3> Consulta de Alunos </h3>
+
+    <table>
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Area</th>
+            <th>Graduação</th>
+</tr>
+
+<tr>
+    <td>1001</td>
+    <td>Rogerio</td>
+    <td>Informatica</td>
+    <td>Informatica com enfase Gestão</td>
+</tr>
+
+<tr>
+    <td>1002</td>
+    <td>Aldy</td>
+    <td>Informatica</td>
+    <td>Ciencias da computação</td>
+</tr>
+
+
+    </table>
+
+</body>
+</html>
